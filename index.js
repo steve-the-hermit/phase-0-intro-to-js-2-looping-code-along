198e75a4b77ac1f6af0c1717f523dd5530ca9920
steve-the-hermit <stevemacharia309@gmail.com>
2023-06-15 19:37:25 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+      thankYouMessages.push(message);
+    }
+    return thankYouMessages;
+  }
+  
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
